4aff288718032208a1119434c9c436fd7b3cc609
Valter Garcia <junior.opto@gmail.com>
2020-01-09 18:44:16 -0300

    added missed files.

---
+++ todo-list/controllers/todoController.js
@@ -0,0 +1,43 @@
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//Connect to the database
+mongoose.connect('mongodb+srv://test:test@cluster0-juezr.mongodb.net/test?retryWrites=true&w=majority');
+
+//Create a schema = this is like a blueprint
+
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+    if (err) throw err;
+    console.log('item saved');
+});
+
+var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kickass some code'}]
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+module.exports = function(app) {
+
+    app.get('/todo', function(req, res){
+        res.render('todo', {todos: data});
+
+    });
+
+    app.post('/todo', urlencodedParser, function(req, res){
+        data.push(req.body);
+        res.json(data);
+
+    });
+
+    app.delete('/todo/:item', function(req, res) {
+        data = data.filter(function(todo){
+            return todo.item.replace(/ /g, '-') !== req.params.item;
+        });
+        res.json(data);
+
+    });
+
+};
